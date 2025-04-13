fb980490809cf3cbac80df9f89a046bd828a3490
Mike Irlam <michael.irlam@hotmail.co.uk>
2025-04-13 15:03:51 +0100

    added all proj files for day-18

---
+++ day-18/README.md
@@ -0,0 +1,9 @@
+# 100 Days of Code - Day 18
+
+This project uses the Python module Turtle Graphics https://docs.python.org/3/library/turtle.html#turtle.goto
+to perform various exercises such as creating a square, dashed line, spirograph etc with the final project recreating
+a famous Hirst painting.
+
+This project didnt really test me like some of the previous ones as it was more cementing learning I'd done in previous
+projects however it did stress the importance of reading documentation and getting familiar with what you can and cant do
+with modules
--- day-18/main.py
+++ day-18/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 import random
+from hirstcolours import hirst_colours
+
 
 leeroy = Turtle()
 leeroy.shape("turtle")
@@ -62,5 +64,36 @@ def spirograph(gap):
         leeroy.circle(100)
         leeroy.left(gap)
 
-spirograph(4)
+# Day 18 Project - Recreating a Hirst painting using Turtle
+
+def line(r):
+    """Takes r as an argument, draws dots in random colours r times across the x axis
+    :params: r - passed in as the range for the for loop to create the line of circles
+    """
+    for num in range(r):
+        leeroy.dot(20, hirst_colours[random.randint(0, 29)])
+        leeroy.forward(50)
+
+# making sure pen is up and speed is set to fastest
+leeroy.penup()
+leeroy.speed(0)
+
+# setting starting position for x & y. Y axis will be reduced by 50 each time the programme loops
+leeroy.setposition(-400.00, 300.00)
+original_x = -400.00
+original_y = 300.00
+
+def make_hirst(x, y, r):
+    """Takes x and y as pos arguements and r as the range for the for loop to recreate a Hirst dot painting
+    :params: x - x axis for the starting position, this position will not change throughout the function
+    :params: y - y axis for the starting position, this will decrease by 50 each loop
+    :params: r - passed in to determine range in loop
+    """
+    for num in range(r):
+        line(r)
+        y -= 50
+        leeroy.setposition(x, y)
+
+
+make_hirst(original_x, original_y, 10)
 screen.exitonclick()
