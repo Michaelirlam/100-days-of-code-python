801b54c31df34081d5237200a09af79e75d6fa3f
Mike Irlam <michael.irlam@hotmail.co.uk>
2025-04-13 21:08:28 +0100

    added game logic for the turtle race

--- day-19/turtle-racing.py
+++ day-19/turtle-racing.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-
+import random
 
 # Turtle objects for the race and their states
 leonardo = Turtle()
@@ -32,6 +32,9 @@ vangogh.shape("turtle")
 vangogh.color("pink")
 vangogh.penup()
 
+turtles = [leonardo, donatello, michelangelo, raphael, davinci, vangogh]
+turtle_colour = ["blue", "purple", "orange", "red", "green", "pink"]
+
 # Logic for drawing the finish line and getting the turtles into their starting position
 
 def draw_finish_line():
@@ -48,16 +51,36 @@ def starting_pos():
     davinci.setpos(-230, -100)
     vangogh.setpos(-230, -160)
 
-draw_finish_line()
-starting_pos()
+
 
 
 # Screen settings
 screen = Screen()
 screen.setup(height=400, width=500)
 
+
+# Game logic
+
+is_race_on = False
+
+draw_finish_line()
+starting_pos()
+
 user_bet = screen.textinput(title="Make your bet!", prompt="Which turtle will win the race? Enter a colour: ")
 
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
+    move = random.randint(0, 10)
+    random.choice(turtles).forward(move)
+    
+    for t in turtles:
+        if t.xcor() >= 230:
+            is_race_on = False
+            if user_bet == turtle_colour[turtles.index(t)]:
+                print("You Won")
+            else:
+                print(f"you Lost, the winner was {turtle_colour[turtles.index(t)]}")
 
 screen.exitonclick()
