bdf51a3d28b5b530f6dc54ce266e8cc63b92feca
Michael Irlam <michaelirlam@hotmail.co.uk>
2025-04-17 08:23:19 +0100

    Created Snake class in snake.py and moved the snake functionality to within the class

--- day-20/main.py
+++ day-20/main.py
@@ -1,46 +1,36 @@
 from turtle import Turtle, Screen
 import time
+from snake import Snake
 
+# Creates the initial snake using the Snake class
+snake = Snake()
+
+def close_game():
+    """function for closing the game, should be used with an onkey event"""
+    global game_over
+    game_over = True
+
+# Screen settings
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-game_over = False
-
-def close_game():
-    global game_over
-    game_over = True
-
-def left():
-    snake_body[0].left(90)
-
-def right():
-    snake_body[0].right(90)
-
-snake_body = []
-for num in range(3):
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.setpos(x= -20 * num, y=0)
-    snake_body.append(new_segment)
-
+# Movement settings
 screen.listen()
 screen.onkey(close_game, "q")
-screen.onkey(left, "Left")
-screen.onkey(right, "Right")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# Game logic
+game_over = False
 
 while not game_over:
     screen.update()
     time.sleep(0.2)
-    for body in range(len(snake_body) - 1, 0, -1):
-        snake_body[body].goto(snake_body[body - 1].pos())
-    snake_body[0].forward(20)
-
-
-
+    snake.move()
+    
 
 
 screen.bye()
