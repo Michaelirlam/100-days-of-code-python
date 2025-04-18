9150651136a0a721c417b7b17c9e465641896fc5
Michael Irlam <michaelirlam@hotmail.co.uk>
2025-04-19 00:15:29 +0100

    Added docstrings to snake class

--- day-20-and-21/snake.py
+++ day-20-and-21/snake.py
@@ -14,27 +14,33 @@ class Snake():
             self.snake_body.append(new_segment)
     
     def left(self):
+        """Moves snake left when assigned to an onkey, stops snake from moving into itself"""
         if self.snake_body[0].heading() != 0:
             self.snake_body[0].setheading(180)
 
     def right(self):
+        """Moves snake right when assigned to an onkey, stops snake from moving into itself"""
         if self.snake_body[0].heading() != 180:
             self.snake_body[0].setheading(0)
     
     def up(self):
+        """Moves snake up when assigned to an onkey, stops snake from moving into itself"""
         if self.snake_body[0].heading() != 270:
             self.snake_body[0].setheading(90)
     
     def down(self):
+        """Moves snake down when assigned to an onkey, stops snake from moving into itself"""
         if self.snake_body[0].heading() != 90:
             self.snake_body[0].setheading(270)
 
     def move(self):
+        """Makes the snake move forward in the direction its facing"""
         for body in range(len(self.snake_body) - 1, 0, -1):
             self.snake_body[body].goto(self.snake_body[body - 1].pos())
         self.snake_body[0].forward(MOVE_DISTANCE)
 
     def grow(self):
+        """Logic to grow the snake"""
         new_segment = Turtle("square")
         new_segment.color("white")
         new_segment.penup()
