42e571fe18e03835e8545c61246d76259bf5b857
Michael Irlam <michaelirlam@hotmail.co.uk>
2025-04-18 19:33:52 +0100

    Added grow logic to snake class and added food and grow to main.py

---
+++ day-20-and-21/snake.py
@@ -0,0 +1,44 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+
+class Snake():
+
+    def __init__(self):
+        self.snake_body = []
+        for num in range(3):
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.setpos(x= -20 * num, y=0)
+            self.snake_body.append(new_segment)
+    
+    def left(self):
+        if self.snake_body[0].heading() != 0:
+            self.snake_body[0].setheading(180)
+
+    def right(self):
+        if self.snake_body[0].heading() != 180:
+            self.snake_body[0].setheading(0)
+    
+    def up(self):
+        if self.snake_body[0].heading() != 270:
+            self.snake_body[0].setheading(90)
+    
+    def down(self):
+        if self.snake_body[0].heading() != 90:
+            self.snake_body[0].setheading(270)
+
+    def move(self):
+        for body in range(len(self.snake_body) - 1, 0, -1):
+            self.snake_body[body].goto(self.snake_body[body - 1].pos())
+        self.snake_body[0].forward(MOVE_DISTANCE)
+
+    def grow(self):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.setpos((self.snake_body[-1].xcor(), self.snake_body[-1].ycor()))
+        self.snake_body.append(new_segment)
+
+    
